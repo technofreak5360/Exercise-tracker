c88567d0c23d0ece2f57d64415562645629b8b4f
mmyadav <mintu08.yadav@gmail.com>
2020-06-07 20:21:02 +0530

    frontend-routes

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class CreateExercise extends Component {
+
+    render() {
+        return (
+            <div><p>create ecercise</p></div>
+        );
+    }
+}
