cb2a7127ae02659b51524f6c804be0462a11cadf
mmyadav <mintu08.yadav@gmail.com>
2020-06-08 17:26:50 +0530

    frontend

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 export default class CreateUser extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+        }
+    }
+
+
+    onChangeUsername = (e) => {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        const user = {
+            username: this.state.username,
+        }
+        console.log(user);
+        axios.post('http://localhost:5000/users/add', user)
+            .then(res => console.log(res.data));
+
+        // window.location = '/';
+        this.setState({
+            username: '',
+        });
+    }
 
     render() {
         return (
-            <div><p>create User</p></div>
+            <div>
+                <h3>Create New User</h3>
+                <form onSubmit={(e) => this.onSubmit(e)}>
+                    <div className="form-group">
+                        <label>Username: </label>
+                        <input type="text"
+                            required
+                            className="form-control"
+                            value={this.state.username}
+                            onChange={(e) => this.onChangeUsername(e)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create User" className="btn btn-primary" />
+                    </div>
+                </form>
+            </div>
         );
     }
 }
